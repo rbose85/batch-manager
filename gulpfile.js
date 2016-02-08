ae059191ca8bc251b54fef79a71bb0fa01a1cacf
Rajiv Bose <rbose85@users.noreply.github.com>
2016-02-08 18:57:58 +0000

    add gulpfile.js, and include dev-dependencies in package.json

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var coveralls = require('gulp-coveralls');
+var eslint = require('gulp-eslint');
+var excludes = require('gulp-exclude-gitignore');
+var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+var nsp = require('gulp-nsp');
+var path = require('path');
+var plumber = require('gulp-plumber');
+
+gulp.task('static', function () {
+  return gulp.src('**/*.js')
+    .pipe(excludes())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('nsp', function (cb) {
+  nsp({ package: path.resolve('package.json') }, cb);
+});
+
+gulp.task('pre-test', function () {
+  return gulp.src('lib/**/*.js')
+    .pipe(excludes())
+    .pipe(istanbul({
+      includeUntested: true
+    }))
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function (cb) {
+  var mochaErr;
+
+  gulp.src('test/**/*.js')
+    .pipe(plumber())
+    .pipe(mocha({ reporter: 'spec' }))
+    .on('error', function (err) {
+      mochaErr = err;
+    })
+    .pipe(istanbul.writeReports())
+    .on('end', function () {
+      cb(mochaErr);
+    });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
+});
+
+gulp.task('coveralls', ['test'], function () {
+  if (!process.env.CI) {
+    return;
+  }
+
+  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+    .pipe(coveralls());
+});
+
+gulp.task('pre-publish', ['nsp']);
+gulp.task('default', ['static', 'test', 'coveralls']);
